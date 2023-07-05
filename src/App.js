1390ba2a335463cab29cdfccbd963b5185f22ed3
almacode1703 <testmygitt.2023@gmail.com>
2023-07-05 21:23:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>React Deployment on Github Pages</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
